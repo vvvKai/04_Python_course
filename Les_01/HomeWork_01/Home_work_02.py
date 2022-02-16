fdd4f8ec2ef4901a43fe4e586aee4b785ab2bcb6
Vitaly <tequilla86@bk.ru>
2022-02-16 23:04:17 +0500

    Домашнее задание к уроку №1

---
+++ Les_01/HomeWork_01/Home_work_02.py
@@ -0,0 +1,10 @@
+time_seconds = int(input('Введите время в секундах: '))
+
+hours_in_seconds = 3600
+minute_in_seconds = 60
+
+hours = time_seconds // hours_in_seconds
+minutes = (time_seconds % hours_in_seconds) // minute_in_seconds
+seconds = (time_seconds % hours_in_seconds) % minute_in_seconds
+
+print(f'Введеное время в формате "чч:мм:сс" составляет: {hours:02d}:{minutes:02d}:{seconds:02d}')
---
+++ Les_01/HomeWork_01/Home_work_03.py
@@ -0,0 +1,7 @@
+num = input('Введите число n: ')
+
+num_n1 = int(num)
+num_n2 = int(num + num)
+num_n3 = int(num + num + num)
+
+print('Сумма чисел n + nn + nnn =', num_n1 + num_n2 + num_n3)
