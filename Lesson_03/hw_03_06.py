4bdc35ebb71fa542a1f3a308004b6e922f8635b5
Vitaly <tequilla86@bk.ru>
2022-02-24 09:39:23 +0500

    ДЗ к уроку №3

--- Lesson_03/hw_03_06.py
+++ Lesson_03/hw_03_06.py
@@ -1,8 +1,18 @@
 # 6.	Реализовать функцию int_func(), принимающую слова из маленьких латинских букв и возвращающую их же,
 # но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
 
-def int_func(input_text):
-    return input_text.title()
 
-print(int_func('text one two true'))
+def int_func(text):
+    if text.islower():
+        text = list(text.split(' '))
+        result = ''
+        for el in text:
+            letter_1st = el[0].upper()
+            el_up = letter_1st + el[1:]
+            result = result + ' ' + str(el_up)
+        return result[1:]
+    else:
+        print('Строка/слово должны состоять из букв нижнего регистра.')
+        return ''
 
+print(int_func('text one two three'))
